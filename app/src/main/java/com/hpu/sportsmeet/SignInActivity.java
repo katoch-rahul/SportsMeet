0a1920c300e7a7dd1c5c627bf61f6d641792bc03
Lost World <katochrahul83@gmail.com>
2019-05-22 17:56:54 +0530

    c3

---
+++ app/src/main/java/com/hpu/sportsmeet/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.hpu.sportsmeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setTitle("Sign In");
+        setContentView(R.layout.activity_sign_in);
+    }
+
+}
